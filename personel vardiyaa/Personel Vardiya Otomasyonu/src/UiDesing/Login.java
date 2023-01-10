ad6acb940db3102a2f5aafdc2878534611ed892a
omerkovl <omerkovl@10.39.161.84>
2023-01-11 01:37:26 +0300

    projeson

---
+++ personel vardiyaa/Personel Vardiya Otomasyonu/src/UiDesing/Login.java
@@ -0,0 +1,163 @@
+
+package UiDesing;
+
+import DataBase.db;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+
+public class Login extends javax.swing.JFrame {
+
+    
+    public Login() {
+        initComponents();
+        
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txt_pass = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        txt_kullanici = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Giriş Sayfası");
+
+        jLabel1.setFont(new java.awt.Font("sansserif", 1, 14)); 
+        jLabel1.setText("Kullanıcı Adı:");
+
+        jLabel2.setFont(new java.awt.Font("sansserif", 1, 14)); 
+        jLabel2.setText("Parola :");
+
+        jButton1.setFont(new java.awt.Font("sansserif", 1, 14)); 
+        jButton1.setText("Giriş Yap");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("sansserif", 3, 18)); 
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Vardiya Otomasyonu Giriş Paneli");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(196, 196, 196)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txt_pass, javax.swing.GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)
+                    .addComponent(txt_kullanici))
+                .addContainerGap(210, Short.MAX_VALUE))
+            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(42, 42, 42)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 36, Short.MAX_VALUE)
+                    .addComponent(txt_kullanici))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txt_pass, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(163, 163, 163))
+        );
+
+        pack();
+    }
+
+   
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        Connection conn = null;
+
+        ResultSet rs = null;
+
+        String kadi = String.valueOf(txt_kullanici.getText());
+        String sifre = String.valueOf(txt_pass.getText().trim());
+        try {
+            conn = db.java_db();
+            rs = conn.createStatement().executeQuery("SELECT * FROM personeller");
+
+            while (rs.next()) {
+
+                if (kadi.equals(String.valueOf(rs.getString("tcNo"))) && sifre.equals(rs.getString("sifre"))) {//kullanıcı adi alırken hata var incele
+                    JOptionPane.showMessageDialog(null, "Giriş Başarılı...");
+
+                    if ("Yonetici".equals(String.valueOf(rs.getString("statu")))) {
+                        YoneticiAnaSayfa frame = new YoneticiAnaSayfa(kadi);
+                        frame.setVisible(true);
+                        this.setVisible(false);
+                        break;
+                    } else {
+                        KullaniciAnaSayfa frame = new KullaniciAnaSayfa(kadi);
+                        frame.setVisible(true);
+                        this.setVisible(false);
+                        break;
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField txt_kullanici;
+    private javax.swing.JPasswordField txt_pass;
+    
+}
